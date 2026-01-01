e4b56af6bf7fbd2eec649beb2b211f33b2ec2b6e
Alessandro Bisi <alessandro.bisi@keyformat.com>
2026-01-01 16:10:20 +0100

    build

--- .open-next/server-functions/default/.next/server/webpack-runtime.js
+++ .open-next/server-functions/default/.next/server/webpack-runtime.js
@@ -6,8 +6,8 @@
        case 4: b(require("./chunks/4.js")); break;
        case 586: b(require("./chunks/586.js")); break;
        case 611: b(require("./chunks/611.js")); break;
-       case 666: b(require("./chunks/666.js")); break;
        case 692: b(require("./chunks/692.js")); break;
+       case 782: b(require("./chunks/782.js")); break;
        case 802: b(require("./chunks/802.js")); break;
        case 805: b(require("./chunks/805.js")); break;
        case 807: b(require("./chunks/807.js")); break;
